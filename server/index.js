eb43c019f99d641a79a26d85ec6a3550e6a23bd1
Elijah Williams <ejwill04@gmail.com>
2020-08-23 15:34:57 -0600

    set up basic client and server  socketio messaging

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
-import express from "express";
-import http from "http";
+import express from 'express';
+import http from 'http';
+import socketio from 'socket.io';
 
-// boilerplate express server
 const app = express();
 const server = http.Server(app);
+const io = socketio(server);
+
+// socket.io handlers
+io.on('connect', socket => {
+  const { id } = socket.client;
+  console.log(`user: ${id}`);
+
+  socket.on('message', msg => {
+    console.log(`${id}: ${msg}`);
+  });
+});
 
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => console.log(`Listening on: ${PORT}`));
